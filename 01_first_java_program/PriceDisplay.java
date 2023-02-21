4c6cdcdf08b2bb09c0a5defce8326d0e0a026dfb
christianpinna <christian.a.pinna@gmail.com>
2023-02-21 15:09:22 +0000

    Added compile_error

---
+++ 01_first_java_program/PriceDisplay.java
@@ -0,0 +1,8 @@
+class PriceDisplay {
+  public static void main(String[] arg) {
+    String item = "Coffee Machine";
+    Double price = 15.99;
+
+    System.out.printf("The price for the %s is %.2f \n", item, price);
+  }
+}
