7a90e2e41170856555bfb3e9ca3a320783ee91bd
christianpinna <christian.a.pinna@gmail.com>
2023-02-27 11:39:54 +0000

    First test pass FizzBuzz

---
+++ tryout/src/test/java/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    void testGenerateListsNumbersUpToOne() {
+        FizzBuzz subject = new FizzBuzz();
+        String result = subject.generate(1);
+        assertEquals("1", result);
+        // Note that in JUnit, the expected value comes first
+        // This is called 'Yoda Conditions'.
+    }
+
+//    @Test
+//    void testGenerateListsNumbersUpToTwo() {
+//        Fizzbuzz subject = new Fizzbuzz();
+//        String result = subject.generate(2);
+//        assertEquals("1, 2", result);
+//    }
+}
